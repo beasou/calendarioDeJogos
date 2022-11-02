8fbd1b38828d80cd367c6d7ba8d4a8e94f354d48
beasou <beatrizreginasouza1@gmail.com>
2022-11-02 00:42:09 -0300

    criando server

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import Fastify from "fastify"
+
+async function start() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    fastify.get('/pools/count', () =>{
+        return {count : 0 }
+    })
+    await fastify.listen({port: 3333})
+}
+
+start()
